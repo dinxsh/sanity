631fef80d6f351fb0c76be6773e92ada156ca375
saurabhchaddha6 <saurabhchaddha6@gmail.com>
2024-07-05 03:41:26 +0530

    Schema and DB connection
    Some  schema and database connection changes

---
+++ lib/dbConnect.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+    isConnected?:number
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void>{
+    if(connection.isConnected){
+        console.log('already connected to database')
+        return
+    }
+
+    try{
+        const db = await mongoose.connect(process.env.MONGODB_URL || '')
+
+        connection.isConnected = db.connections[0].readyState
+
+        console.log("DB Connected successfully")
+    }
+    catch(error){
+        console.log("Database connection failed")
+
+        process.exit(1)
+    }
+}
+
+export default dbConnect;
