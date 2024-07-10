4c49bed48d5915f9832c5c905ccb07c6626d585d
saurabhchaddha6 <saurabhchaddha6@gmail.com>
2024-07-10 20:10:54 +0530

    sign-in completed

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -27,7 +27,7 @@ export const authOptions: NextAuthOptions = {
           if (!user) {
             throw new Error('No user found with this email');
           }
-          if (user.twoFactorActivated) {
+          if (!user.twoFactorActivated) {
             throw new Error('Please verify your account first');
           }
           const isPasswordCorrect = await bcrypt.compare(credentials.password, user.password);
