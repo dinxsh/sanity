d4120144df6d7b48c9f7c6549c52497ddcd21cc0
aneesazc <anees.azc@gmail.com>
2024-08-06 16:45:11 +0530

    save to db feature for brackets

--- model/Bracket.js
+++ model/Bracket.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const BracketSchema = new Schema({
     bracketName: { type: String, required: true },
-    tournamentId: { type: Schema.Types.ObjectId, ref: 'Tournament', required: true }, // Make this optional
+    tournamentId: { type: Schema.Types.ObjectId, ref: 'Tournament', required: true },
     bracketImage: String,
     bracketData: String,
     createdAt: { type: Date, default: Date.now }
