7479a1dcad7248471d1f64bc7474176b7346ef07
Abhishek <abhishekbhosale.dev@gmail.com>
2024-08-07 19:39:04 +0530

    style: bracket generator page
    improved ui and styling of bracket page

--- app/bracket/page.js
+++ app/bracket/page.js
@@ -1,14 +1,15 @@
 import TournamentBracket from "../../components/TournamentBracket";
-import BracketList from "../../components/BracketList";
 
 const BracketPage = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-bold text-center my-8">Tournament Manager</h1>
-            <TournamentBracket />
-            <BracketList />
-        </div>
-    );
+  return (
+    <div className="min-h-[70vh] px-7">
+      <h1 className="mt-20 mb-10 text-3xl font-semibold tracking-tight text-center">
+        Tournament Manager
+      </h1>
+
+      <TournamentBracket />
+    </div>
+  );
 };
 
 export default BracketPage;
