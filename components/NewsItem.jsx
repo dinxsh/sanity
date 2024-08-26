c3cf7d5a68948c4dcbb87cdb2a16da955ebb43e4
saurabhchaddha6 <saurabhchaddha6@gmail.com>
2024-08-26 22:06:49 +0530

    improved ui news page

--- components/NewsItem.jsx
+++ components/NewsItem.jsx
@@ -2,31 +2,33 @@ import React from "react";
 
 const NewsItem = ({ title, description, url, urlToImage }) => {
   return (
-    <div className="flex-1 p-6 md:p-10 z-10 overflow-auto">
-      <div className=" flex bg-black rounded-2xl border border-solid border-indigo-700 ">
-        <div className=" p-10 z-10 overflow-hidden">
-          <div className="min-h-96 ">
-            <div className="relative h-64">
-              <img
-                src={urlToImage}
-                alt={urlToImage}
-                className="w-full h-full rounded-sm object-cover"
-              />
-              <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent h-24"></div>
-            </div>
-            <h3 className="text-2xl cursor-pointer font-bold mb-4 text-neon-green hover:text-white transition-colors duration-300 line-clamp-3">
-              <a href={url}>{title}</a>
-            </h3>
-            <p className="text-gray-300 mb-6 text-base line-clamp-4">
-              {description}
-            </p>
-            <a href={url} className="inline-flex items-center justify-center  text-white py-2" >Read Full Article</a>
-          </div>
+    <div className="w-72 h-72 p-4">
+      <div className="bg-black rounded-xl border border-indigo-700 h-full flex flex-col overflow-hidden">
+        <div className="relative h-1/2">
+          <img
+            src={urlToImage}
+            alt={title}
+            className="w-full h-full object-cover"
+          />
+          <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black to-transparent h-12"></div>
+        </div>
+        <div className="p-4 flex flex-col justify-between flex-grow">
+          <h3 className="text-lg font-bold mb-2 text-neon-green hover:text-white transition-colors duration-300 line-clamp-2">
+            <a href={url}>{title}</a>
+          </h3>
+          <p className="text-gray-300 text-sm mb-2 line-clamp-2">
+            {description}
+          </p>
+          <a 
+            href={url} 
+            className="text-white text-sm hover:text-neon-green transition-colors duration-300"
+          >
+            Read Full Article
+          </a>
         </div>
       </div>
     </div>
   );
 };
-
 
 export default NewsItem;
