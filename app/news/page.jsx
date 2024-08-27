ea0f0ae393295df2aa7846afec9f3d716c58814c
dinesh <talwadkerdinesh@gmail.com>
2024-08-27 09:54:56 +0530

    ui(news): index, cards & banners

--- app/news/page.jsx
+++ app/news/page.jsx
@@ -3,17 +3,11 @@ import News from '../../components/News'
 
 const page = async () => {
     return(
-        <div className="flex flex-col mt-5 gap-7">
-       <div className="bg-cover bg-center bg-opacity-50 bg-[url(https://im.indiatimes.in/facebook/2019/Oct/mobile_gaming_1570096408.jpg)] w-full flex flex-col justify-center text-center items-center pl-7 h-[500px] gap-3">
-        <h1 className="text-5xl font-extrabold justify-center"> Welcome to {' '}
-        <span className='text-transparent bg-clip-text bg-gradient-to-r from-[#4F46E5] to-[#E114E5]'> 
-             Esports News
-            </span> </h1>
-        <span className="auto text-md">Your Ultimate source for Esports News</span>
-      </div>
-      <News/>
-      </div>
-
+        <div className="flex flex-col">
+            <div className="bg-cover bg-center bg-[url(https://im.indiatimes.in/facebook/2019/Oct/mobile_gaming_1570096408.jpg)] w-full flex flex-col justify-center items-center h-[200px] py-8 px-4">
+            </div>
+            <News/>
+        </div>
     )
 }
 
