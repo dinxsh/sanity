d87dd9af2fb17c4c9990f28a919dfcb624a9842e
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-08-29 13:52:17 +0530

    added few packages

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,53 @@
-import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 import { ThemeProvider } from "../components/theme-provider";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import AuthProvider from "../context/AuthProvider";
 import NextTopLoader from "nextjs-toploader";
+import dotenv from "dotenv";
 
-const inter = Inter({ subsets: ["latin"] });
+dotenv.config();
+
+const helvetica = localFont({
+  src: [
+    {
+      path: "../fonts/helvetica/HelveticaNowDisplay-Thin.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../fonts/helvetica/HelveticaNowDisplay-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../fonts/helvetica/HelveticaNowDisplay-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../fonts/helvetica/HelveticaNowDisplay-Bold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../fonts/helvetica/HelveticaNowDisplay-ExtraBold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../fonts/helvetica/HelveticaNowDisplay-Black.woff2",
+      weight: "800",
+      style: "normal",
+    },
+    {
+      path: "../fonts/helvetica/HelveticaNowDisplay-ExtBlk.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata = {
   title: "Sanity E-Sports",
@@ -17,7 +58,7 @@ export default function RootLayout({ children }) {
   return (
     <AuthProvider>
       <html lang="en">
-        <body className={inter.className}>
+        <body className={helvetica.className}>
           <ThemeProvider
             attribute="class"
             defaultTheme="system"
