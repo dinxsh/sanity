1736337271d0aff66d8740a715ab61bd1bc9279f
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-09-02 17:32:36 +0530

    tweak config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,13 +7,11 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    colors: {
+      primary : "#000000",
+      secondary: "#ffffff",
+      accent: "#d00000"
+    }
   },
   plugins: [],
 };
