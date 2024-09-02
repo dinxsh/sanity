de5880e4e56cb67992eace1babe3471e04f4e2f8
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-09-02 17:33:01 +0530

    configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,37 +5,37 @@ import localFont from "next/font/local";
 const helvetica = localFont({
   src: [
     {
-      path: "../fonts/helvetica/HelveticaNowDisplay-Thin.woff2",
+      path: "../../fonts/helvetica/HelveticaNowDisplay-Thin.woff2",
       weight: "300",
       style: "normal",
     },
     {
-      path: "../fonts/helvetica/HelveticaNowDisplay-Regular.woff2",
+      path: "../../fonts/helvetica/HelveticaNowDisplay-Regular.woff2",
       weight: "400",
       style: "normal",
     },
     {
-      path: "../fonts/helvetica/HelveticaNowDisplay-Medium.woff2",
+      path: "../../fonts/helvetica/HelveticaNowDisplay-Medium.woff2",
       weight: "500",
       style: "normal",
     },
     {
-      path: "../fonts/helvetica/HelveticaNowDisplay-Bold.woff2",
+      path: "../../fonts/helvetica/HelveticaNowDisplay-Bold.woff2",
       weight: "600",
       style: "normal",
     },
     {
-      path: "../fonts/helvetica/HelveticaNowDisplay-ExtraBold.woff2",
+      path: "../../fonts/helvetica/HelveticaNowDisplay-ExtraBold.woff2",
       weight: "700",
       style: "normal",
     },
     {
-      path: "../fonts/helvetica/HelveticaNowDisplay-Black.woff2",
+      path: "../../fonts/helvetica/HelveticaNowDisplay-Black.woff2",
       weight: "800",
       style: "normal",
     },
     {
-      path: "../fonts/helvetica/HelveticaNowDisplay-ExtBlk.woff2",
+      path: "../../fonts/helvetica/HelveticaNowDisplay-ExtBlk.woff2",
       weight: "900",
       style: "normal",
     },
