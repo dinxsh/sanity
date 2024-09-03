9caf2d4cbff6c9a76bcdb5136ef53ba83089d80d
Zade Scoxp <78777405+zadescoxp@users.noreply.github.com>
2024-09-03 23:12:30 +0530

    Footer introduced needs changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 const helvetica = localFont({
   src: [
@@ -58,6 +59,7 @@ export default function RootLayout({
       <body className={helvetica.className}>
         <Navbar />
         {children}
+        {/* <Footer /> */}
       </body>
     </html>
   );
