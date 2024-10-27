6e89c8b6b5699d7e14b3b0a9a5b8c744450321cf
dinesh <talwadkerdinesh@gmail.com>
2024-10-28 04:51:54 +0530

    fix(prod err): must provide string spec

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
     images: {
         remotePatterns: [
           {
@@ -9,29 +8,6 @@ const nextConfig = {
           },
         ],
       },
-=======
-  images: {
-    domains: ["cdn.sanity.io"],
-    remotePatterns: [
-      // {
-      //   protocol: "https",
-      //   hostname: "media.battlexo.com",
-      //   port: "",
-      //   pathname: "/**",
-      // },
-      // {
-      //   protocol: "https",
-      //   hostname: "sanityesports.live",
-      //   port: "",
-      //   pathname: "/**",
-      // },
-      {
-        protocol: "https",
-        hostname: "*",
-      },
-    ],
-  },
->>>>>>> c64d4052a8a28cb66fc7ed964cea672fd032d412
 };
 
 export default nextConfig;
