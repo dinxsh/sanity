ee4a366a0c0d8ae16b20a585aacf5593b82ab283
Advait <76993204+777advait@users.noreply.github.com>
2024-12-05 21:55:16 +0530

    feat: add brackets viewer

--- app/api/brackets/[id]/route.js
+++ app/api/brackets/[id]/route.js
@@ -1,18 +1,19 @@
 import dbConnect from '../../../../lib/dbConnect';
 import Bracket from '../../../../model/Bracket';
 import { NextResponse } from 'next/server';
+import mockBrackets from './mockBrackets';
 
 export async function GET(request, { params }) {
     const { id } = params;
     console.log('Fetching bracket with ID:', id);
     try {
         await dbConnect();
-        const bracket = await Bracket.findById(id);
-        if (!bracket) {
-            console.log('Bracket not found');
-            return NextResponse.json({ error: 'Bracket not found' }, { status: 404 });
-        }
-        return NextResponse.json(bracket);
+        // const bracket = await Bracket.findById(id);
+        // if (!bracket) {
+        //     console.log('Bracket not found');
+        //     return NextResponse.json({ error: 'Bracket not found' }, { status: 404 });
+        // }
+        return NextResponse.json(mockBrackets);
     } catch (error) {
         console.error('Error fetching bracket:', error);
         return NextResponse.json({ error: 'Internal Server Error', details: error.message }, { status: 500 });
---
+++ components/Brackets.jsx
@@ -0,0 +1,51 @@
+"use client";
+
+import { useEffect } from "react";
+import "brackets-viewer/dist/brackets-viewer.min.js";
+import "brackets-viewer/dist/brackets-viewer.min.css";
+import "./styles/bracket.css";
+
+async function render(id) {
+  //refer to Template.md file to spin up this path.
+  const data = await fetch(`/api/brackets/${id}`).then((res) => res.json());
+
+  if (typeof window !== "undefined") {
+    window.bracketsViewer.setParticipantImages(
+      data.participant.map((participant) => ({
+        participantId: participant.id,
+        imageUrl: "https://github.githubassets.com/pinned-octocat.svg",
+      }))
+    );
+
+    window.bracketsViewer.render(
+      {
+        stages: data.stage,
+        matches: data.match,
+        matchGames: data.match_game,
+        participants: data.participant,
+      },
+      { clear: true },
+      {
+        participantOriginPlacement: "before",
+        separatedChildCountLabel: true,
+        showSlotsOrigin: true,
+        showLowerBracketSlotsOrigin: true,
+        highlightParticipantOnHover: true,
+      }
+    );
+  }
+}
+
+function Bracket({id}) {
+  useEffect(() => {
+    render(id);
+  }, []);
+
+  return (
+    <>
+      <div className="brackets-viewer custom"></div>
+    </>
+  );
+}
+
+export default Bracket;
