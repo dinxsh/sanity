5a59a1d1d3e865b7423a4076f8f617276de3b686
sanyam <sanyammadaanfca@gmail.com>
2024-12-17 00:34:18 +0530

    Improved news page ui

--- components/News.jsx
+++ components/News.jsx
@@ -46,46 +46,35 @@ const News = () => {
   };
 
   const renderNewsSlider = (title, articles, category) => (
-    <div className="mb-16">
-      <h2 className="text-[50px] text-tertiary font-semibold mb-8 border-b-[1px] border-tertiary pb-2">
+    <div className="mb-16 mt-2">
+      <div className="uppercase text-white text-center text-2xl md:text-3xl h-fit font-semibold ">
         {title}
-      </h2>
+      </div>
 
-      <div className="relative">
-        <button
-          onClick={() => scroll(category, "left")}
-          className="absolute -left-2 top-0 bg-gradient-to-r from-primary to-transparent text-secondary text-[20px] px-5 h-full w-auto z-10"
-        >
-          <FaChevronLeft />
-        </button>
+      <div>
         <div
           ref={sliderRefs[category]}
-          className="flex overflow-x-hidden space-x-6 pb-8"
+          className="grid grid-cols-1 p-2 m-2 md:grid-cols-2 md:p-1 md:m-1 "
         >
           {articles.map((article, index) => (
-            <div key={index} className="flex-none">
+            <div key={index} className="flex flex-col  justify-between border border-gray-700 shadow-md border-b-0 bg-gray-800 dark:border-gray-600 my-10 mx-5  ">
+                <div className="">
                 <NewsItem
                   title={article.title}
                   description={article.description}
                   url={article.url}
                   urlToImage={article.image}
                 />
+                </div>
             </div>
           ))}
         </div>
-        <button
-          onClick={() => scroll(category, "right")}
-          className="absolute -right-2 top-0 bg-gradient-to-l from-primary to-transparent text-secondary text-[20px] px-5 h-full w-auto z-10"
-        >
-          <FaChevronRight />
-        </button>
       </div>
     </div>
   );
 
   return (
-    <div className="container mx-auto px-8 py-16">
-      <h1 className="text-6xl font-bold mb-16 text-center">News Highlights</h1>
+    <div className="container px-2 h-fit ">
       {renderNewsSlider("Latest News", latestNews, "latestNews")}
       {renderNewsSlider("Esports", esportsNews, "esportsNews")}
       {renderNewsSlider("Gaming News", gamingNews, "gamingNews")}
