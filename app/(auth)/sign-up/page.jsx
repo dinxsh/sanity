40ac135c2d47ee73935079b1ce8149005dbb5058
zzzzshawn <zzzzshawn@gmail.com>
2024-12-17 17:12:57 +0530

    Auth ui revamp

--- app/(auth)/sign-up/page.jsx
+++ app/(auth)/sign-up/page.jsx
@@ -25,10 +25,11 @@ import {
 } from "../../../@/components/ui/card";
 
 import { useToast } from "../../../@/components/ui/use-toast";
-import axios from "axios"
+import axios from "axios";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { signUpSchema } from "../../../model/Schema/signUpSchema";
+import Image from "next/image";
 
 const Page = () => {
   const [username, setUsername] = useState("");
@@ -103,101 +104,130 @@ const Page = () => {
 
   return (
     <div>
-    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: "url('/pexels-lulizler-3165335.jpg')" }}>  
-    <div className="flex justify-center items-center min-h-[70vh] pt-16">
-      <Card className="w-96">
-        <CardHeader>
-          <CardTitle className="text-2xl md:text-3xl tracking-tight">
-            Welcome to Sanity
-          </CardTitle>
-          <CardDescription>
-            Sign up to start your gaming adventure
-          </CardDescription>
-        </CardHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <CardContent className="flex flex-col gap-5 mt-5">
-              <FormField
-                name="username"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Username</FormLabel>
-                    <Input
-                      {...field}
-                      onChange={(e) => {
-                        field.onChange(e);
-                        debouncedUsername(e.target.value);
-                      }}
-                    />
-                    {isCheckingUsername && <Loader2 className="animate-spin" />}
-                    {!isCheckingUsername && usernameMessage && (
-                      <p
-                        className={`text-sm ${
-                          usernameMessage === "Username is unique"
-                            ? "text-green-500"
-                            : "text-red-500"
-                        }`}
-                      >
-                        {usernameMessage}
-                      </p>
+      <div className="bg-cover bg-center ">
+        <div className="flex justify-center items-center">
+          <Card className="w-[23rem] max-sm:w-[19rem] border-zinc-400/10">
+            <CardHeader className="p-2">
+              <CardDescription className="flex flex-col gap-2 pt-5 items-center justify-center">
+                <Image
+                  src="/assets/logo.jpg"
+                  alt=""
+                  width={500}
+                  height={500}
+                  className="size-12 rounded-xl"
+                />
+                <p className="font-bold text-base">Create an account.</p>
+                <p className="text-zinc-400">
+                  Welcome! Please fill the details to get started.
+                </p>
+              </CardDescription>
+            </CardHeader>
+            <Form {...form}>
+              <form onSubmit={form.handleSubmit(onSubmit)}>
+                <CardContent className="space-y-5">
+                  <FormField
+                    name="username"
+                    control={form.control}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-base font-bold text-zinc-300">
+                          Username
+                        </FormLabel>
+                        <Input
+                          className="text-base border-zinc-400/10"
+                          {...field}
+                          onChange={(e) => {
+                            field.onChange(e);
+                            debouncedUsername(e.target.value);
+                          }}
+                        />
+                        {isCheckingUsername && (
+                          <Loader2 className="animate-spin" />
+                        )}
+                        {!isCheckingUsername && usernameMessage && (
+                          <p
+                            className={`text-sm ${
+                              usernameMessage === "Username is unique"
+                                ? "text-green-500"
+                                : "text-red-500"
+                            }`}
+                          >
+                            {usernameMessage}
+                          </p>
+                        )}
+                        <FormMessage />
+                      </FormItem>
                     )}
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                  />
 
-              <FormField
-                name="email"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Email</FormLabel>
-                    <Input {...field} name="email" />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
+                  <FormField
+                    name="email"
+                    control={form.control}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-base font-bold text-zinc-300">
+                          Email
+                        </FormLabel>
+                        <Input
+                          className="text-base border-zinc-400/10"
+                          {...field}
+                          name="email"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
 
-              <FormField
-                name="password"
-                control={form.control}
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>Password</FormLabel>
-                    <Input type="password" {...field} name="password" />
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-            </CardContent>
-            <CardFooter className="flex flex-col gap-2">
-              <Button type="submit" className="w-full" disabled={isSubmitting}>
-                {isSubmitting ? (
-                  <>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                    Please wait
-                  </>
-                ) : (
-                  "Sign Up"
-                )}
-              </Button>
+                  <FormField
+                    name="password"
+                    control={form.control}
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel className="text-base font-bold text-zinc-300">
+                          Password
+                        </FormLabel>
+                        <Input
+                          className="text-base border-zinc-400/10"
+                          type="password"
+                          {...field}
+                          name="password"
+                        />
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </CardContent>
+                <CardFooter className="flex flex-col gap-2">
+                  <Button
+                    type="submit"
+                    className="w-full font-bold"
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? (
+                      <>
+                        <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                        Please wait
+                      </>
+                    ) : (
+                      "Sign Up"
+                    )}
+                  </Button>
 
-              <div className="text-center mt-4 text-foreground/70 transition-all">
-                Already a member?{" "}
-                <Link
-                  href="/sign-in"
-                  className="hover:text-foreground underline transition-all"
-                >
-                  Sign in
-                </Link>
-              </div>
-            </CardFooter>
-          </form>
-        </Form>
-      </Card>
-    </div>
-    </div>
+                  <div className="mt-5 text-foreground/80 text-xs text-center text-zinc-400">
+                    Already a member?{" "}
+                    <Link
+                      href="/sign-in"
+                      className="hover:text-blue-800 underline transition-all "
+                    >
+                      Sign in
+                    </Link>
+                  </div>
+                </CardFooter>
+              </form>
+            </Form>
+          </Card>
+        </div>
+      </div>
     </div>
   );
 };
