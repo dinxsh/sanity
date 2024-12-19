9ccb48348ebddb3852fd38c6d2a0ee8ea775836c
Ankit <kashyap25ankit@gmail.com>
2024-12-19 23:34:21 +0530

    revamed the teams page -- added search func

--- components/TeamCard.jsx
+++ components/TeamCard.jsx
@@ -1,63 +1,62 @@
+import { Gamepad2, Languages, Router, ShieldAlert, Users } from "lucide-react";
+import Image from "next/image";
 import React from "react";
+import { MdLeaderboard } from "react-icons/md";
+import { Button } from "./ui/button";
+import { Separator } from "../@/components/ui/separator";
 
 const TeamCard = ({ team }) => {
   return (
-    <div className="p-4 bg-slate-800 text-white mb-4 rounded-md">
-      <div className="border-b-4 border-b-stone-600">
-        <div className="flex justify-between items-center mb-5">
-          <div className="flex items-center">
-            <img
-              src={team.image || "https://via.placeholder.com/40"}
-              alt={`${team.name} logo`}
-              className="w-10 h-10 rounded-full mr-4"
-            />
-            <h3 className="text-xl">{team.teamname}</h3>
-          </div>
-          <button className="bg-transparent border-2 border-blue-800 px-4 py-2 rounded hidden md:block">
-            Request to join
-          </button>
+    <div className="flex  flex-col gap-6 bg-[#0E1728] rounded-lg  p-6 mx-auto ">
+      <div>
+        <Image
+          src={team.image || "/static-team-logo.png"}
+          width={500}
+          height={500}
+          alt="team-img"
+          className="rounded-lg w-full h-36"
+        />
+        <p className="text-2xl font-bold mt-2">{team.teamname}</p>
+      </div>
+
+      <div className="flex gap-4 flex-wrap">
+        <div className="flex gap-2 items-center text-gray-400">
+          <Gamepad2 />
+          <p>{team.game}</p>
+        </div>
+
+        <div className="flex gap-2 items-center text-gray-400">
+          <ShieldAlert />
+          <p>{team.role}</p>
+        </div>
+
+        <div className="flex gap-2 items-center text-gray-400">
+          <MdLeaderboard />
+          <p>{team.rank}</p>
+        </div>
+
+        <div className="flex gap-2 items-center text-gray-400">
+          <Router />
+          <p>{team.rank}</p>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-6 gap-x-4 my-5">
-        <div className="my-2">
-          <span className="text-yellow-500">Game</span>
-          <span className="block">{team.game}</span>
+
+      <Separator className="bg-gray-400" />
+
+      <div className="flex justify-between flex-wrap">
+        <div className="flex gap-2 items-center text-gray-400">
+          <Users />
+          <span className="block">{team.players.join(" , ")}</span>
         </div>
-        <div className="my-2">
-          <span className="text-yellow-500">Role</span>
-          <span className="block">{team.role}</span>
-        </div>
-        <div className="my-2">
-          <span className="text-yellow-500">Rank</span>
-          <span className="block">{team.rank}</span>
-        </div>
-        <div className="my-2">
-          <span className="text-yellow-500">Server</span>
-          <span className="block">{team.server}</span>
-        </div>
-        <div className="my-2">
-          <span className="text-yellow-500">Language</span>
+
+        <div className="flex gap-2 items-center text-gray-400">
+          <Languages />
           <span className="block">{team.language}</span>
         </div>
-        <div className="my-2">
-          <span className="text-yellow-500">Participants</span>
-          <span className="block">{team.participantCount}</span>
-        </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-5 gap-x-4 my-5">
-        <div className="my-2">
-          <span className="text-yellow-500">Players</span>
-          <span className="block">{team.players.join(", ")}</span>
-        </div>
-        <div className="my-2">
-          <span className="text-yellow-500">Looking for</span>
-          <span className="block">{team.requests}</span>
-        </div>
-      </div>
-      <div className="flex justify-center md:hidden">
-        <button className="bg-transparent border-2 border-blue-800 px-4 py-2 rounded">
-          Request to join
-        </button>
+
+      <div>
+        <Button className="w-full">Request to Join</Button>
       </div>
     </div>
   );
