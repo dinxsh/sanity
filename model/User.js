4cd94fb7c066cbb2fca01baa0d3915d085201a98
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2024-12-20 14:38:58 +0530

    feat(user): add discordId field to user schema

--- model/User.js
+++ model/User.js
@@ -29,6 +29,10 @@ const userSchema = new Schema({
     type: String,
     default: "",
   },
+  discordId: {
+    type: String,
+    default: "",
+  },
   twoFactorActivated: {
     type: Boolean,
     default: false,
