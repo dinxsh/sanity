0442a051466cde746f7220851fb1a9d1c49ad65f
chirag <chiragdave1888@gmail.com>
2024-12-26 00:56:14 +0530

    updated the tournament-bracket route(incomplete)

--- app/api/tournament-bracket/route.js
+++ app/api/tournament-bracket/route.js
@@ -2,27 +2,40 @@ import { NextResponse } from "next/server";
 import dbConnect from "../../../lib/dbConnect";
 import Tournament from "../../../model/Tournament";
 import Bracket from "../../../model/Bracket";
+import { InMemoryDatabase } from "brackets-memory-db";
+import { BracketsManager } from "brackets-manager";
+import { StageType } from "brackets-model";
 
 export async function POST(request) {
   const { tournament_id } = await request.json();
+  const storage = new InMemoryDatabase();
+  const manager = new BracketsManager(storage);
 
   try {
     await dbConnect();
     const tournament = await Tournament.findById(tournament_id);
+    if (!tournament) {
+      return NextResponse.json(
+        { error: "Tournament not found" },
+        { status: 404 },
+      );
+    }
 
     await manager.create.stage({
-      tournament_id,
+      tournamentId: tournament_id,
       name: tournament.tournamentName,
-      type: tournamentFormat,
+      type: tournament.tournamentFormat,
       seeding: tournament.teamsRegistered.map((team) => {
         return { id: team.id, name: team.name, tournament_id };
       }),
     });
 
+    console.log(await manager.get.tournamentData(tournament_id));
+
     const newBracket = await Bracket({
       ...(await manager.get.tournamentData(tournament_id)),
       tournamentName: tournament.tournamentName,
-      format: format,
+      format: tournament.tournamentFormat,
     });
 
     await newBracket.save();
