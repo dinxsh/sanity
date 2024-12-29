69efc403797cceca16fe290867f8291a7573f64f
Kuzuri247 <mindsetmatters47@gmail.com>
2024-12-29 17:00:31 +0530

    analytics

--- app/page.js
+++ app/page.js
@@ -1,17 +1,3 @@
-// import HeroSection from "../components/HeroSection";
-// import FeatureSection from "../components/FeatureSection/FeatureSection";
-// import FaqSection from "../components/FaqSection";
-
-// export default async function Home() {
-//   return (
-//     <main className="">
-//       <HeroSection />
-//       <FeatureSection />
-//       <FaqSection />
-//     </main>
-//   );
-// }
-// pages/index.js
 import HeroSection from "../components/HeroSection";
 import FeatureSection from "../components/FeatureSection/FeatureSection";
 import FaqSection from "../components/FaqSection";
